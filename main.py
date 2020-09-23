e45a26d182ec50ac728cd03f862d9a8e92530a0e
EC2 Default User <ec2-user@ip-172-31-24-154.ec2.internal>
2020-09-23 00:15:58 +0000

    Restyle and reformat html. Add functions to get data easier

--- main.py
+++ main.py
@@ -26,29 +26,51 @@ food_items = ['potatoes','broccoli','garlic','tomatoes','pasta','tacos','ice cre
 
 @app.route('/')
 def index():
-    
+    count = 5
     query = food_items[random.randint(0,7)]
-    results = getTweets(query, 3)
+    results = getTweets(query, count)
+    texts = getTexts(results)
+    times = getTimes(results)
+    users = getUsers(results)
     return flask.render_template(
         "index.html",
-        Results = results,
-        list_len = 3
+        Texts = texts,
+        Users = users,
+        Times = times,
+        list_len = count
         )
-        
-        
+def getUsers(results):
+    users = []
+    for tweet in results:
+        users.append(tweet.user.name)
+    return users
+    
+def getTexts(results):
+    tweets = []
+    for tweet in results:
+        tweets.append(tweet.text)
+    return tweets
+    
 def getTweets(query, count):
     response = api.search(query, count = count, show_user = True, lang = "en")
     tweets = []
     for tweet in response:
-        time = getTime(tweet.created_at)
-        tweets.append(str(tweet.user.name) + ' tweeted: "' + tweet.text + '" at: ' +str(tweet.created_at.month) +' '+str(tweet.created_at.day)+', '+str(tweet.created_at.year)+', '+time)
+        tweets.append(tweet)
     return tweets
     
-def getTime(time):
-    hour = str((time.hour - 4) % 12)
-    minute = str(time.minute)
-    second = str(time.second)
-    return (hour+':'+minute+':'+second+' EST' )
+def getTimes(results):
+    times = []
+    for tweet in results:
+        time = tweet.created_at
+        year = str(time.year)
+        month = str(time.month)
+        day = str(time.day)
+        hour = str((time.hour))
+        minute = str(time.minute)
+        second = str(time.second)
+        holder = month+'/'+day+'/'+year+' at '+hour+':'+minute+':'+second+ ' UCT'
+        times.append(holder)
+    return times
     
 app.run(
     port = int(os.getenv("PORT", 8080)),   
