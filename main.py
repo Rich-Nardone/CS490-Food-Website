6c134fd68266baf80563b9303dd6eb50d5cb129e
EC2 Default User <ec2-user@ip-172-31-24-154.ec2.internal>
2020-09-28 14:07:11 +0000

    Add code to make sure recipe contains an image

--- main.py
+++ main.py
@@ -36,27 +36,28 @@ state = ['Italian','Chinese','Mexican','Greek','Indian','Japanese','Thai','Cajun
 @app.route('/')
 def index():
     rnum = random.randint(0,8)
-    randnum = random.randint(0,19) 
     count = 1
     cuisine = state[rnum]
     response = requests.get(surl+"&cuisine="+state[rnum]+"&addRecipeInformation=true&number=20")
     json_body = response.json()
-    url = (json.dumps(json_body["results"][randnum]["image"])).replace("\"","")
+    while True:
+        randnum = random.randint(0,19)
+        try:
+            url = (json.dumps(json_body["results"][randnum]["image"])).replace("\"","")
+        except:
+            pass
+        else: 
+            break
     recipeID = json.dumps(json_body["results"][randnum]["id"])
     ingredients = getIngredients(recipeID)
     Query = json.dumps(json_body["results"][randnum]["title"]).replace("\"","")
-    query = cuisine+" cuisine OR food OR snack"
+    query = cuisine+" cuisine OR food OR snack -filter:retweets"
     results = getTweets(query, count)
-    texts = getTexts(results)
-    times = getTimes(results)
-    users = getUsers(results)
-    #urls = getUrls(results)
     return flask.render_template(
         "index.html",
-        Texts = texts,
-        Users = users,
-        Times = times,
-        #Urls = urls,
+        Texts = getTexts(results),
+        Users = getUsers(results),
+        Times = getTimes(results),
         Cuisine = cuisine,
         Recipe = Query,
         list_len = count,
@@ -66,7 +67,7 @@ def index():
         )
         
 def getTweets(query, count):
-    response = api.search(query, count = 1, show_user = True, lang = "en")
+    response = api.search(query, count = 1, show_user = True, result_type = "popular",lang = "en")
     tweets = []
     for tweet in response:
         tweets.append(tweet)
@@ -82,7 +83,8 @@ def getTexts(results):
     tweets = []
     for tweet in results:
         status = api.get_status(tweet.id_str, tweet_mode="extended")
-        tweets.append(status.full_text)
+        text = (status.full_text)
+        tweets.append(text)
     return tweets
     
 def getTimes(results):
@@ -113,5 +115,6 @@ def getIngredients(ID):
 
 app.run(
     port = int(os.getenv("PORT", 8080)),   
-    host = os.getenv("IP", "0.0.0.0")
+    host = os.getenv("IP", "0.0.0.0"),
+    debug=True
 )
